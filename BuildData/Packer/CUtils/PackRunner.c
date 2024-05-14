e20878a82fc48d4c88e0f0832881850f84f90f60
Mark Redmann <markredmann@icloud.com>
2024-05-14 02:48:22 +0000

    packer-run: Fully featured implementation

--- BuildData/Packer/CUtils/PackRunner.c
+++ BuildData/Packer/CUtils/PackRunner.c
@@ -22,26 +22,20 @@
 // THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                //
 ///////////////////////////////////////////////////////////////////////////////
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <limits.h>
 #include <stdbool.h>
 #include <libgen.h>
 #include <string.h>
 #include <sys/mount.h>
-#define _GNU_SOURCE
 #include <sched.h>
-
-//////////////// NOTE: /////////////////
-/// This code should NEVER and will NEVER
-/// be used in production. This is just being
-/// made quickly so I can test out Packer.
-////////////////////////////////////////
-
 
 /// CONSTANTS:
 ////////////////////////////////////////
-#define INTERNAL_PATH_MAX 2048
+#define INTERNAL_PATH_MAX 1024
 
 /// USAGE:
 ////////////////////////////////////////
@@ -57,8 +51,121 @@ void Usage(const char* ExecName)
     printf("\n");
     printf("  --- Usage: %s /Packages/Binaries/program_name\n", ExecName);
     printf("\n");
+    printf("  --- Usage: %s /Packages /bin/sh\n", ExecName);
+    printf("\n");
     exit(1);
 }
+
+/// SAFECHDIR:
+////////////////////////////////////////
+void Chdir_Or_Die(const char* Dir) {
+    if ( chdir(Dir) == -1 ) {
+        printf(" --- [PACKER]: Cannot change working directory to \"%s\".\n", Dir);
+        perror(" --- [PERROR]: chdir() ");
+        exit(1);
+    }
+}
+
+/// RUN:MOUNTS:
+////////////////////////////////////////
+void Run_Mounts(const char* Packer_Path,
+                const char* Packer_Root,
+                const char* Packer_Env) {
+    if ( unshare(CLONE_NEWNS) == -1 ) {
+        printf(" --- [PACKER]: Failed to create new Mount namespace.\n");
+        perror(" --- [PERROR]: unshare() : ");
+        exit(1);
+    }
+    
+    //////////////// NOTE: /////////////////
+    /// It doesnt really matter if any of these fail,
+    /// so don't check them.
+    ////////////////////////////////////////
+    const char* OPTS = "bind,x-gvfs-hide";
+    mount("/Applications",   "Applications/",   NULL, MS_BIND | MS_REC, OPTS);
+    mount("/Packages",       "Packages/",       NULL, MS_BIND | MS_REC, OPTS);
+    mount("/Mount",          "Mount/",          NULL, MS_BIND | MS_REC, OPTS);
+    mount("/System",         "System/",         NULL, MS_BIND | MS_REC, OPTS);
+    mount("/Users",          "Users/",          NULL, MS_BIND | MS_REC, OPTS);
+    mount("/",               ".sysroot/",       NULL, MS_BIND | MS_REC, OPTS);
+
+    // Mount Env overlay
+    // Adding +1 removes the leading /
+    mount(Packer_Env,         (Packer_Path + 1),   NULL, MS_BIND | MS_REC, OPTS);
+    // Mount Raw Env
+    mount(Packer_Path,         ".rawenv/",        NULL, MS_BIND | MS_REC, OPTS);
+
+}
+
+/// RUN:PROGRAM:
+////////////////////////////////////////
+#define ERR_RUN_INVALID_BIN 20
+unsigned Run_Program(uid_t UID,
+                     const char*  Packer_Path,
+                     const char*  Program_Path, 
+                     const char** Program_Args)
+{
+/// Common Variables
+    char Packer_Root[INTERNAL_PATH_MAX];
+    char Packer_Env[INTERNAL_PATH_MAX];
+    char WorkingDirectory[INTERNAL_PATH_MAX];
+    int  Result;
+/// Initialise Packer_Root
+    Result = snprintf(Packer_Root, INTERNAL_PATH_MAX, "%s/.root", Packer_Path);
+    if ( Result > INTERNAL_PATH_MAX ) {
+        printf(" --- [PACKER]: Resolving Packer Directory \"%s\" internals exceeds (%u) INTERNAL_PATH_MAX (%u)\n",
+                Packer_Path, Result, INTERNAL_PATH_MAX);
+        perror(" --- [PERROR]: snprintf() ");
+        exit(1);
+    }
+/// Initialise Packer_Env
+    Result = snprintf(Packer_Env, INTERNAL_PATH_MAX, "%s/.root/.packerenv", Packer_Path);
+    if ( Result > INTERNAL_PATH_MAX ) {
+        printf(" --- [PACKER]: Resolving Packer Directory \"%s\" internals exceeds (%u) INTERNAL_PATH_MAX (%u)\n",
+                Packer_Path, Result, INTERNAL_PATH_MAX);
+        perror(" --- [PERROR]: snprintf() ");
+        exit(1);
+    }
+
+/// Initialise CWD
+    if ( getcwd(WorkingDirectory, INTERNAL_PATH_MAX) == 0 ) {
+        printf(" --- [PACKER]: Cannot get current working directory.\n");
+        perror(" --- [PERROR]: getcwd() ");
+        exit(1);
+    }
+
+/// Run Mounts
+    Chdir_Or_Die(Packer_Root);
+    Run_Mounts(Packer_Path, Packer_Root, Packer_Env);
+    Chdir_Or_Die(WorkingDirectory);
+/// Change Root
+    if ( chroot(Packer_Root) == -1 ) {
+        printf(" --- [PACKER]: Failed to change root to \"%s\".\n", Packer_Root);
+        perror(" --- [PERROR]: chroot() ");
+        exit(1);
+    }
+    // Drop permissions
+    if ( setuid(UID) == -1 ) {
+        printf(" --- [PACKER]: Failed to change UID to \"%u\".\n", UID);
+        perror(" --- [PERROR]: setuid() ");
+        exit(1);
+    }
+    
+    Chdir_Or_Die("/");              // Inside of Packer Root
+    Chdir_Or_Die(WorkingDirectory); // Should now be relative to Packer Root
+/// Run Executable
+    execv(Program_Path, (char**)(Program_Args));
+    /// --- This should never be reached! --- ///
+    printf(" --- [PACKER]: The specified binary \"%s\" could not be executed within the Packer Environment.\n",
+           Program_Path);
+    perror(" --- [PERROR]: execv() ");
+    exit(1);
+    
+}
+
+
+/// PARSE:
+////////////////////////////////////////
 
 /// PARSE:FILE:
 ////////////////////////////////////////
@@ -66,11 +173,8 @@ void Usage(const char* ExecName)
 #define PARSE_CANT_OPEN        1
 #define PARSE_NO_EMBEDDED_PATH 2
 #define PARSE_OVERRUN          3
-#define PARSE_CANT_NORMALISE   4
-#define PARSE_CANT_CHDIR       5
-#define PARSE_NO_PACKERENV     6
 
-unsigned ParseFile(const char* FilePath, char* Output, unsigned OutputLen)
+unsigned Parse_File(const char* FilePath, char* Output, unsigned OutputLen)
 {
     FILE* File = fopen(FilePath, "r");
     if ( File == NULL )
@@ -111,189 +215,139 @@ unsigned ParseFile(const char* FilePath, char* Output, unsigned OutputLen)
     return 0;
 }
 
-/// PARSE:PACKERENV:
+
+
+/// PARSE:MANUAL:
 ////////////////////////////////////////
-// unsigned ParsePackerEnv(char* FilePath, char* Output, unsigned OutputLen)
-// {
-//     //////////////// FIXME: ////////////////
-//     /// This, is stupid. It works for now.
-//     ////////////////////////////////////////
-    
-//     char* Dirname = dirname(FilePath);
-//     if ( Dirname == 0 )
-//         return PARSE_CANT_NORMALISE;
-
-//     if ( chdir(Dirname) != 0 )
-//         return PARSE_CANT_CHDIR;
-
-//     if ( chdir("../.root") != 0 )
-//         return PARSE_NO_PACKERENV;
-    
-//     if ( getcwd(Output, OutputLen) == 0 )
-//         return PARSE_NO_PACKERENV;
-    
-//     return 0;
-// };
-
-/// PARSE:PACKERDIR:
-////////////////////////////////////////
-unsigned Parse_PackerDir(char* FilePath,
-                         char* DirOut, 
-                         char* EnvOut,
-                         unsigned DirOutLen,
-                         unsigned EnvOutLen)
+unsigned Parse_Manual(uid_t UID,
+                      const char*  Packer_Dir,
+                      const char*  Packer_Bin,
+                      const char** Bin_Args) 
 {
-    //////////////// FIXME: ////////////////
-    /// This, is stupid. It works for now.
+    /// Variables
     ////////////////////////////////////////
-
-    char* Dirname = dirname(FilePath);
-
-    if ( Dirname == 0 )
-        return PARSE_CANT_NORMALISE;
-
-    if ( chdir(Dirname) != 0 )
-        return PARSE_CANT_CHDIR;
-
-    chdir("../");
-
-    if ( getcwd(DirOut, DirOutLen) == 0 )
-        return PARSE_NO_PACKERENV;
-
-    snprintf(EnvOut, EnvOutLen, "%s/.root/.packerenv/", DirOut);
-
-    return PARSE_OK;
+    char Packer_RealPath[INTERNAL_PATH_MAX];
+    int Result;
+    
+    // Validate Packer Directory
+    if ( realpath(Packer_Dir, Packer_RealPath) == NULL ) {
+        printf(" --- [PACKER]: Failed to resolve Packer Directory \"%s\".\n",
+                Packer_Dir);
+        perror(" --- [PERROR]: realpath() ");
+        exit(1);
+    }
+    return Run_Program(UID, Packer_RealPath, Packer_Bin, Bin_Args);
 }
 
-/// RUNMOUNTS:
+/// PARSE:NORMAL:
 ////////////////////////////////////////
-void Run_Mounts(const char* PackerDir, const char* PackerEnv)
-{
-    unshare(CLONE_NEWNS);
+unsigned Parse_Normal(uid_t UID, 
+                      const char*  FilePath,
+                      const char** Program_Args) {
+    /// Common Variables
+    char     Temp[INTERNAL_PATH_MAX];
+    char     Temp2[INTERNAL_PATH_MAX];
+    char     Packer_Path[INTERNAL_PATH_MAX];
+    char     Program_Path[INTERNAL_PATH_MAX];
+    unsigned Result;
+
+    /// Parse the File
+    Result = Parse_File(FilePath, Program_Path, INTERNAL_PATH_MAX);
+    switch ( Result ) {
+        case PARSE_CANT_OPEN:
+            printf(" --- [PACKER]: Failed to open Program Link \"%s\".\n", FilePath);
+            perror(" --- [PERROR]: fopen ");
+            exit(1);
+        break;
+        
+        case PARSE_NO_EMBEDDED_PATH:
+            printf(" --- [PACKER]: Program Link \"%s\" does not contain an embedded path.\n", FilePath);
+            exit(1);
+        break;
+
+        case PARSE_OVERRUN:
+            printf(" --- [PACKER]: Embedded path in Program Link \"%s\" exceeds INTERNAL_PATH_MAX (%u)\n",
+                   FilePath, INTERNAL_PATH_MAX);
+            exit(1);
+        break;
+    }
+    /// Normalise FilePath
+    if ( realpath(FilePath, Temp) == NULL ) {
+        printf(" --- [PACKER]: Failed to resolve Program Link path \"%s\".\n",
+                Temp);
+        perror(" --- [PERROR]: realpath() ");
+        exit(1);
+    }
+    if ( dirname(Temp) == NULL ) {
+        printf(" --- [PACKER]: Failed to resolve Program Link dirname \"%s\".\n",
+                Temp);
+        perror(" --- [PERROR]: dirname() ");
+        exit(1);
+    }
     
-    //////////////// NOTE: /////////////////
-    /// It doesnt really matter if any of these fail,
-    /// so don't check them.
-    ////////////////////////////////////////
-    const char* OPTS = "bind,x-gvfs-hide";
-    mount("/Applications",   "Applications/",   NULL, MS_BIND | MS_REC, OPTS);
-    mount("/Packages",       "Packages/",       NULL, MS_BIND | MS_REC, OPTS);
-    mount("/Mount",          "Mount/",          NULL, MS_BIND | MS_REC, OPTS);
-    mount("/System",         "System/",         NULL, MS_BIND | MS_REC, OPTS);
-    mount("/Users",          "Users/",          NULL, MS_BIND | MS_REC, OPTS);
-    mount("/",               ".sysroot/",       NULL, MS_BIND | MS_REC, OPTS);
+    /// Retrieve Packer directory from FilePath (After dirname'd to Temp2)
+    Result = snprintf(Temp2, INTERNAL_PATH_MAX, "%s/../", Temp);
+    if ( Result > INTERNAL_PATH_MAX ) {
+        printf(" --- [PACKER]: Attempting to retrieve Packer Directory from Program Link base directory \"%s\" internals exceeds (%u) INTERNAL_PATH_MAX (%u)\n",
+                Temp, Result, INTERNAL_PATH_MAX);
+        perror(" --- [PERROR]: snprintf() ");
+        exit(1);
+    }
+    
+    if ( realpath(Temp2, Packer_Path) == NULL ) {
+        printf(" --- [PACKER]: Failed to resolve Packer Directory \"%s\".\n",
+                Temp);
+        perror(" --- [PERROR]: realpath() ");
+        exit(1);
+    }
 
-    // Mount Env overlay
-    // Adding +1 removes the leading /
-    mount(PackerEnv,         (PackerDir + 1),   NULL, MS_BIND | MS_REC, OPTS);
-    // Mount Raw Env
-    mount(PackerDir,         ".rawenv/",        NULL, MS_BIND | MS_REC, OPTS);
+    /// Run Program
+    Run_Program(UID, Packer_Path, Program_Path, Program_Args);
 
+    return 0;
 }
+
+/// ENTRY POINT
+////////////////////////////////////////
 
 
 /// MAIN:
 ////////////////////////////////////////
-
-int main(int argc, const char** argv)
-{
-    /// Setup Commons
-    ////////////////////////////////////////
-    
-    if ( argc < 2) // Fail if there is no passed binary.
+int main(int argc, const char** argv) {
+/// Sanity Check
+////////////////////////////////////////
+    if ( argc < 2 )
         Usage(argv[0]);
 
-    const char* InputPath = argv[1];
-    char  InputCopy[INTERNAL_PATH_MAX];
-    strncpy(InputCopy, InputPath, INTERNAL_PATH_MAX);
-    
     /// Validate this is a SetUID Binary
-    ////////////////////////////////////////
-
     if ( geteuid() != 0 ) {
-        printf(" --- [X]: Cannot execute \"%s\" because the runner utility \"%s\" is not set as SetUID.\n", argv[1], argv[0]);
-        printf("         Run \"chmod u+s\" on the runner utility to fix this issue.\n");
+        printf(" --- [PACKER]: Cannot execute \"%s\" because the runner utility \"%s\" is not set as SetUID.\n", argv[1], argv[0]);
+        printf("               Run \"chmod u+s\" on the runner utility to fix this issue.\n");
         exit(1);
     }
 
     /// Save UIDs
-    ////////////////////////////////////////
     uid_t UID = getuid();
     if ( setuid(0) < 0) {
-        printf(" --- [X]: Cannot setuid. Does the runner utility have the setuid bit set?\n");
+        printf(" --- [PACKER]: Cannot setuid. Does the runner utility have the setuid bit set?\n");
+        perror(" --- [PERROR]: setuid() ");
         exit(1);
     }
     
-    /// Parse File.
-    ////////////////////////////////////////
-    unsigned Result;
-    char     PackerBinPath[INTERNAL_PATH_MAX];
+/// If argc is 3 it means the 1st parameter is a path to the Packer
+/// directory, and the 2nd parameter is a path to a binary to execute.
+///////////////////////////////////////////////////////////////////////
+    bool MANUAL_EXECUTION = false;
+    unsigned Error;
+    if ( argc >= 3 )
+        MANUAL_EXECUTION = true;
 
-    Result = ParseFile(InputPath, PackerBinPath, INTERNAL_PATH_MAX);
-    
-    switch ( Result ) {
-        case PARSE_CANT_OPEN:
-            printf(" --- [X]: Cannot execute \"%s\" because the file cannot be opened or does not exist.\n", InputPath);
-            exit(1);
-        break;
-        case PARSE_NO_EMBEDDED_PATH:
-            printf(" --- [X]: Cannot execute \"%s\" because the file is malformed and/or does not contain an embedded path.\n", InputPath);
-            exit(1);
-        break;
-        case PARSE_OVERRUN:
-            printf(" --- [X]: Cannot execute \"%s\" because the file is malformed and causes an internal failure.\n", InputPath);
-        break;
-    }
+/// Run Program
+    if ( MANUAL_EXECUTION )
+        Error = Parse_Manual(UID, argv[1], argv[2], argv + 2);
+    else
+        Error = Parse_Normal(UID, argv[1], argv + 1);
 
-    /// Parse Packer root
-    ////////////////////////////////////////
-    char PackerDir[INTERNAL_PATH_MAX];
-    char PackerEnv[INTERNAL_PATH_MAX];    
-    char CWD[INTERNAL_PATH_MAX];
-
-    if ( getcwd(CWD, INTERNAL_PATH_MAX) == 0 ) {
-        printf(" --- [X]: Cannot get current working directory.\n");
-        exit(1);
-    }
-
-    Result = Parse_PackerDir(InputCopy, PackerDir, PackerEnv, 
-                             INTERNAL_PATH_MAX, INTERNAL_PATH_MAX);
-
-    switch ( Result ) {
-        case PARSE_CANT_NORMALISE:
-            printf(" --- [X]: Cannot normalise \"%s\".\n", InputPath);
-            exit(1);
-        break;
-        case PARSE_CANT_CHDIR:
-            printf(" --- [X]: Cannot find internal directories. Is Packer setup correctly?\n");
-            exit(1);
-        break;
-        case PARSE_NO_PACKERENV:
-            printf(" --- [X]: No Packer environment was found. Is Packer setup correctly?\n");
-            exit(1);
-        break;
-    }
-
-    /// Setup mounts
-    ////////////////////////////////////////
-    chdir(PackerDir);
-    chdir(".root/");
-    Run_Mounts(PackerDir, PackerEnv);
-
-    /// Switch to Packer Root
-    ////////////////////////////////////////
-    chroot(".");
-    chdir("/");
-    setuid(UID); // Dropped!
-    chdir(CWD);  // Relative to chroot
-
-    /// Run the program
-    //////////////////////////////////////
-    execv(PackerBinPath, (char**)(argv + 1));
-    /// --- This should never be reached! --- ///
-    printf(" --- [X]: The specified binary \"%s\" cannot be found within the Packer environment.\n", PackerBinPath);
-
-    
-
-
+/// UNREACHABLE ///
+    return 1;
 }
